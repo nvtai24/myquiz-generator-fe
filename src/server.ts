6a9407fa1e70d583e5bad48a6de44a15a3c41d7c
Nguyễn Võ Thái Bảo <baonguyenvothai@gmail.com>
2026-02-27 10:34:43 +0700

    feat: update size limits  add allowed host

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine({
-  allowedHosts: ['localhost'],
+  allowedHosts: ['localhost', 'learn.myquiz.fun'],
 });
 
 app.use(
