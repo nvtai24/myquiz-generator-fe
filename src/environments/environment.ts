9f68634752c726bfc1b5d2d97766ae94bfc2f9b9
Tai Nguyen Van <buiblue123@gmail.com>
2026-03-26 16:19:37 +0700

    fix: change to tenten domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://be.myquiz.fun',
+  apiUrl: 'https://myquiz.nvtai24.site',
   googleClientId: '725294510426-h2tpulq6s0736t1s76il9rt5sda715dc.apps.googleusercontent.com'
 };
